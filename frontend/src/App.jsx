60e6fcf5cd41bb8219e357ff60f2727a7b3463d2
Abhishek-Marethiya <168818112+Abhishek-Marethiya@users.noreply.github.com>
2024-11-23 00:00:25 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
     const handleSubmit = async () => {
         try {
             const parsedInput = JSON.parse(input);
-            const response = await fetch("bajaj-api-task-three.vercel.app/bfhl
+            const response = await fetch("http://bajaj-api-task-ten.vercel.app//bfhl
 ", {
                 method: "POST",
                 headers: {
